b0e8179c56bff79e92f9ba5f483c2890621b816d
Chaudhari <Chaudhari@DESKTOP-M3C7DC6>
2021-06-30 16:19:40 -0600

    Set-up Models Finished setting up models (User and Role).

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -11,4 +11,72 @@ package models;
  */
 public class User {
     
+    private String email;
+    private boolean isActive;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private int roleId;
+    
+    public User() {
+        
+    }
+    
+    public User(String email, boolean isActive, String firstName, String lastName, String password, int roleId) {
+        this.email = email;
+        this.isActive = isActive;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.roleId = roleId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+    
 }
