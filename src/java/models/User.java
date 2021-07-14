dc8277a4112c1aad9aa80b41b2adafc1e4b2af63
Chaudhari <Chaudhari@DESKTOP-M3C7DC6>
2021-07-14 15:53:27 -0600

    Replacing JDBC with JPA Added JPA library and Entity classes for tables.

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -6,31 +6,67 @@
 package models;
 
 import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
  * @author Chaudhari
  */
+@Entity
+@Table(name = "user")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u")
+    , @NamedQuery(name = "User.findByEmail", query = "SELECT u FROM User u WHERE u.email = :email")
+    , @NamedQuery(name = "User.findByActive", query = "SELECT u FROM User u WHERE u.active = :active")
+    , @NamedQuery(name = "User.findByFirstName", query = "SELECT u FROM User u WHERE u.firstName = :firstName")
+    , @NamedQuery(name = "User.findByLastName", query = "SELECT u FROM User u WHERE u.lastName = :lastName")
+    , @NamedQuery(name = "User.findByPassword", query = "SELECT u FROM User u WHERE u.password = :password")})
 public class User implements Serializable {
-    
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "email")
     private String email;
-    private boolean isActive;
+    @Basic(optional = false)
+    @Column(name = "active")
+    private boolean active;
+    @Basic(optional = false)
+    @Column(name = "first_name")
     private String firstName;
+    @Basic(optional = false)
+    @Column(name = "last_name")
     private String lastName;
+    @Basic(optional = false)
+    @Column(name = "password")
     private String password;
-    private int roleId;
-    
+    @JoinColumn(name = "role", referencedColumnName = "role_id")
+    @ManyToOne(optional = false)
+    private Role role;
+
     public User() {
-        
     }
-    
-    public User(String email, boolean isActive, String firstName, String lastName, String password, int roleId) {
+
+    public User(String email) {
         this.email = email;
-        this.isActive = isActive;
+    }
+
+    public User(String email, boolean active, String firstName, String lastName, String password) {
+        this.email = email;
+        this.active = active;
         this.firstName = firstName;
         this.lastName = lastName;
         this.password = password;
-        this.roleId = roleId;
     }
 
     public String getEmail() {
@@ -41,12 +77,12 @@ public class User implements Serializable {
         this.email = email;
     }
 
-    public boolean isActive() {
-        return isActive;
+    public boolean getActive() {
+        return active;
     }
 
-    public void setIsActive(boolean isActive) {
-        this.isActive = isActive;
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
     public String getFirstName() {
@@ -73,12 +109,37 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    public int getRoleId() {
-        return roleId;
+    public Role getRole() {
+        return role;
     }
 
-    public void setRoleId(int roleId) {
-        this.roleId = roleId;
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (email != null ? email.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.email == null && other.email != null) || (this.email != null && !this.email.equals(other.email))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "models.User[ email=" + email + " ]";
     }
     
 }
